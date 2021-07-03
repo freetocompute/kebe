6d42a3c748e41c2353455eb653631dbb184546dd
Tim Howard <timothy.howard@gmail.com>
2021-07-03 17:04:28 -0400

    Remove unused macaroon code

--- pkg/auth/macaroon.go
+++ pkg/auth/macaroon.go
@@ -2,47 +2,8 @@ package auth
 
 import (
 	"encoding/base64"
-	"fmt"
 	macaroonv2 "gopkg.in/macaroon.v2"
 )
-
-// Generate - Generate a Macaroon from the Token configuration
-func Generate(secret string, t *Token) (*macaroonv2.Macaroon, error){
-	// create a new macaroon
-	m, err := macaroonv2.New([]byte(secret), []byte(t.ID), t.Location, macaroonv2.Version(t.Version))
-	if err != nil {
-		return nil, fmt.Errorf("error creating new macaroon: %w", err)
-	}
-
-	for _, caveat := range t.ThirdPartyCaveats {
-		for _, c := range caveat {
-			bs := []byte(c.Id)
-			err := m.AddThirdPartyCaveat([]byte(secret),
-				bs, c.Location)
-			if err != nil {
-				return nil, fmt.Errorf("failed to add caveat: %w", err)
-			}
-		}
-	}
-
-	return m, nil
-}
-
-type Caveat struct {
-	Id string
-	Location string
-}
-
-// Caveats - configuration representation of key pair caveats
-type Caveats []Caveat
-
-// Token - configuration representation of token metadata attributes
-type Token struct {
-	ID                string
-	Version           int
-	Location          string
-	ThirdPartyCaveats []Caveats
-}
 
 // MacaroonSerialize returns a store-compatible serialized representation of the given macaroon
 func MacaroonSerialize(m *macaroonv2.Macaroon) (string, error) {
